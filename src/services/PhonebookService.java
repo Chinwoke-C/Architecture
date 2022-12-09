8b47b5d911cf1c376734e1757afcca992f41616c
chinwoke <canugwara99@gmail.com>
2022-12-09 09:59:43 +0100

    new commit

--- src/services/PhonebookService.java
+++ src/services/PhonebookService.java
@@ -3,9 +3,9 @@ package services;
 import data.models.Contact;
 
 public interface PhonebookService {
-    void register();
-    void addContact();
-    Contact findContact();
+    void register(String ownersName, String ownersPhoneNumber);
+    void addContact(String ownersPhoneNumber, String firstName, String lastName, String phoneNumber );
+    Contact findContact(String ownersPhoneNumber, String phoneNumberToSearchFor);
     void delete();
     void updateContact();
 }
--- src/services/PhonebookServiceImpl.java
+++ src/services/PhonebookServiceImpl.java
@@ -19,6 +19,21 @@ public class PhonebookServiceImpl  implements PhonebookService{
     }
 
     @Override
+    public void register(String ownersName, String ownersPhoneNumber) {
+
+    }
+
+    @Override
+    public void addContact(String ownersPhoneNumber, String firstName, String lastName, String phoneNumber) {
+
+    }
+
+    @Override
+    public Contact findContact(String ownersPhoneNumber, String phoneNumberToSearchFor) {
+        return null;
+    }
+
+    @Override
     public void delete() {
 
     }
